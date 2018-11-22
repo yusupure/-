b950d4a45b8b417ba18d0034e8d6c80a8bcb5692
yusupure <44675048+yusupure@users.noreply.github.com>
2018-11-22 23:16:53 +0800

    Update zhihu.py

--- allfullspidersearch/allfullspidersearch/spiders/zhihu.py
+++ allfullspidersearch/allfullspidersearch/spiders/zhihu.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+import json
 
+from allfullspidersearch.dataitem.zhihu.zhihulogin.zhihusigin import zhihu_login
+
+
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    #start_urls = ['https://www.zhihu.com']
+    url='https://www.zhihu.com/api/v3/feed/topstory/recommend?session_token=e6cd631e8aed41f5a4a7e5be48a05cce&desktop=true&limit=7&action=down&after_id=5'
+    def start_requests(self):
+        yield Request(url=self.url,meta={'dont_redirect':True,'handle_httpstatus_list':[401,301]},callback=self.parse)
+ 
+    def parse(self, response):
+        jsonlist=json.loads(response.text)
+        if 'code' in jsonlist.keys():
+            yield Request(url='https://www.zhihu.com',callback=zhihu_login())#更新过期COOKIES
+    def parer_deltie(self,response):
+        pass
