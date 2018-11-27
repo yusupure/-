67ee0d2b7e2abddb884b2a86a58ab253a5306b23
yusupure <44675048+yusupure@users.noreply.github.com>
2018-11-27 16:54:33 +0800

    Update zhihu_items.py

--- allfullspidersearch/allfullspidersearch/dataitem/zhihu/zhihu_items.py
+++ allfullspidersearch/allfullspidersearch/dataitem/zhihu/zhihu_items.py
@@ -1,8 +1,15 @@
 import scrapy
 from elasticsearch_dsl import connections
-from allfullspidersearch.dataitem.zhihu.zhihu_es_sql import ZhuhuArticleType
+import datetime
+
+from w3lib.html import remove_tags
+
+from allfullspidersearch.dataitem.zhihu.zhihu_es_sql import ZhuhuArticleType, ZhiHuAnswerIndex
 from allfullspidersearch.dataset.dataset import MD5_sh
 import re
+
+from allfullspidersearch.settings import SQL_DATETIME_FORMAT,SQL_DATE_FORMAT
+
 es = connections.connections.create_connection(ZhuhuArticleType._doc_type.using, hosts = '192.168.7.126')
 
 class ZhihuItemLoader(scrapy.Item):
@@ -65,9 +72,85 @@ class ZhihuItemLoader(scrapy.Item):
 
 
 
-# class zhihusjsonItemLoader(scrapy.item):
-#     zhihuid=scrapy.Field()
-#     zhihu_content=scrapy.Field()
-#     zhihu_title=scrapy.Field()
-#     url_token=scrapy.Field()
-#     follow_name=scrapy.Field()
+class zhihusjsonItemLoader(scrapy.Item):
+    url_object_id = scrapy.Field()
+    answer_id = scrapy.Field()
+    question_id = scrapy.Field()
+    author_id = scrapy.Field()
+    author_name = scrapy.Field()
+    content = scrapy.Field()
+    praise_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    url = scrapy.Field()
+    update_time = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+    def clean_data(self):
+        #self["create_time"] = datetime.datetime.fromtimestamp(self["create_time"]).strftime(SQL_DATE_FORMAT)
+        #self["update_time"] = datetime.datetime.fromtimestamp(self["update_time"]).strftime(SQL_DATE_FORMAT)
+        self["crawl_time"] = self["crawl_time"].strftime(SQL_DATE_FORMAT)
+        self["content"] = remove_tags(self["content"])
+
+    def save_to_mysql(self):
+        # 插入知乎answer表的sql语句
+        insert_sql = """
+                   insert into zhihu_answer(url_object_id, answer_id, question_id, author_id, author_name,
+                   content, praise_num, comments_num,url,create_time,
+                   update_time, crawl_time)
+                   VALUES (%s, %s, %s, %s, %s,
+                      %s, %s, %s, %s, %s,
+                      %s, %s)
+                     ON DUPLICATE KEY UPDATE
+                     content=VALUES(content), comments_num=VALUES(comments_num), praise_num=VALUES(praise_num),
+                     update_time=VALUES(update_time), author_name=VALUES(author_name)
+               """
+        self.clean_data()
+        sql_params = (
+            self["url_object_id"], self["answer_id"], self["question_id"], self["author_id"], self["author_name"],
+            self["content"], self["praise_num"], self["comments_num"], self["url"], self["create_time"],
+            self["update_time"], self["crawl_time"])
+
+        return insert_sql, sql_params
+
+    def gen_suggest(self,index, info_tuple):
+        # 根据字符串生成所搜建议数据
+        # python重要性titel:10
+        used_words = set()
+        suggest = []
+        for text, weight in info_tuple:
+            if text:
+                # 调用es的analyze借口分析字符串
+                words = es.indices.analyze(index = index, analyzer = "ik_max_word", params = {'filter': ['lowercase']},
+                                           body = text)
+                anylzed_words = set([r['token'] for r in words['tokens'] if len(r["token"]) > 1])
+                new_words = anylzed_words - used_words
+            else:
+                new_words = set()
+            if new_words:
+                suggest.append({"input": list(new_words), "weight": weight})
+        return suggest
+
+    def elasticsearch_es_sql(self):
+        self.clean_data()
+        zhihu = ZhiHuAnswerIndex()
+
+        zhihu.meta.id = self["url_object_id"]
+        zhihu.answer_id = self["answer_id"]
+        zhihu.question_id = self["question_id"]
+        zhihu.author_id = self["author_id"]
+        zhihu.author_name = self["author_name"]
+
+        zhihu.content = self["content"]
+        zhihu.praise_num = self["praise_num"]
+        zhihu.comments_num = self["comments_num"]
+        zhihu.url = self["url"]
+        #zhihu.create_time = self["create_time"]
+
+        #zhihu.update_time = self["update_time"]
+        zhihu.crawl_time = self["crawl_time"]
+
+        # 在保存数据时便传入suggest
+        zhihu.suggest = self.gen_suggest(ZhuhuArticleType._doc_type.index,
+                                          ((zhihu.author_name, 10), (zhihu.content, 7)))
+        #real_time_count("zhihu_answer_count", ZHIHU_QUESTION_COUNT_INIT)
+        zhihu.save()
