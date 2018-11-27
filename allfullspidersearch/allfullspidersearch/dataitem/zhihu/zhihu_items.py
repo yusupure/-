59dfcb8acddbe736d7689b2d81cdefd2ced5fe58
yusupure <44675048+yusupure@users.noreply.github.com>
2018-11-27 12:00:33 +0800

    Update zhihu_items.py

--- allfullspidersearch/allfullspidersearch/dataitem/zhihu/zhihu_items.py
+++ allfullspidersearch/allfullspidersearch/dataitem/zhihu/zhihu_items.py
@@ -1,15 +1,9 @@
-
 import scrapy
-from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst,Join,MapCompose
-
-# def return_value(value):
-#     return value
-#
-# class zhihunewItemloader(ItemLoader):
-#     default_output_processor = TakeFirst()
+from elasticsearch_dsl import connections
+from allfullspidersearch.dataitem.zhihu.zhihu_es_sql import ZhuhuArticleType
 from allfullspidersearch.dataset.dataset import MD5_sh
 import re
+es = connections.connections.create_connection(ZhuhuArticleType._doc_type.using, hosts = '192.168.7.126')
 
 class ZhihuItemLoader(scrapy.Item):
     zhihu_url_id=scrapy.Field()#Id
@@ -18,9 +12,7 @@ class ZhihuItemLoader(scrapy.Item):
     zhihu_tags=scrapy.Field()#标识
     zhihu_itemInner=scrapy.Field()#关注人
     zhihu_item=scrapy.Field()#浏览次数
-    zhihu_detail = scrapy.Field()
-    zhihu_headerText=scrapy.Field()#回答数
-    zhihu_unescapable=scrapy.Field()#回答内容
+
     def clean_data(self):
         self['zhihu_url_id'] = MD5_sh(self['zhihu_url_id'][0])
         self['zhihu_title'] = self['zhihu_title'][0]  # 标题
@@ -30,22 +22,46 @@ class ZhihuItemLoader(scrapy.Item):
         self['zhihu_item'] = int(self['zhihu_item'][1])  # 浏览次数
 
     def save_sql_es(self):
-        insert_sql='''
+        insert_sql="""
             insert into zhihunew(zhihu_url_id,zhihu_title,zhihu_commer,zhihu_tags,zhihu_itemInner,zhihu_item)
             values (%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE zhihu_commer=VALUES(zhihu_commer),
             zhihu_itemInner=VALUES(zhihu_itemInner),zhihu_item=VALUES(zhihu_item)
-        '''
-        # zhihu_url_id = MD5_sh(self['zhihu_url_id'][0])
-        # zhihu_title = self['zhihu_title'][0]  # 标题
-        # zhihu_commer =int(re.findall(r'(\d+).*',self['zhihu_commer'][0],re.S)[0])# 评论数
-        # zhihu_tags = ','.join(self['zhihu_tags'])# 标识
-        # zhihu_itemInner = int(self['zhihu_itemInner'][0])  # 关注人
-        # zhihu_item = int(self['zhihu_item'][1])  # 浏览次数
+        """
         self.clean_data()
         parmer=(self['zhihu_url_id'],self['zhihu_title'],self['zhihu_commer'],self['zhihu_tags'],
                 self['zhihu_itemInner'],self['zhihu_item'])
         return (insert_sql,parmer)
 
+    def gen_suggest(self,index, info_tuple):
+        # 根据字符串生成所搜建议数据
+        # python重要性titel:10
+        used_words = set()
+        suggest = []
+        for text, weight in info_tuple:
+            if text:
+                # 调用es的analyze借口分析字符串
+                words = es.indices.analyze(index = index, analyzer = "ik_max_word", params = {'filter': ['lowercase']},
+                                           body = text)
+                anylzed_words = set([r['token'] for r in words['tokens'] if len(r["token"]) > 1])
+                new_words = anylzed_words - used_words
+            else:
+                new_words = set()
+            if new_words:
+                suggest.append({"input": list(new_words), "weight": weight})
+        return suggest
+
+
+    def elasticsearch_es_sql(self):
+        self.clean_data()
+        Article=ZhuhuArticleType()
+        Article.zhihu_url_id=self['zhihu_url_id']
+        Article.zhihu_title=self['zhihu_title']
+        Article.zhihu_commer=self['zhihu_commer']
+        Article.zhihu_tags=self['zhihu_tags']
+        Article.zhihu_itemInner=self['zhihu_itemInner']
+        Article.zhihu_item=self['zhihu_item']
+        Article.suggest=self.gen_suggest(ZhuhuArticleType._doc_type.index,((Article.zhihu_title,10),(Article.zhihu_item,7)))
+        Article.save()
 
 
 
